7ae537c4274bda26cb4da0fd75c1937eeaafac13
Crowdbotics <team@crowdbotics.com>
2023-11-16 17:57:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "consult-spark-44594",
+  url: "https://consult-spark-44594.botics.co",
+  api: "https://consult-spark-44594.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
